6a046f6a6dacd38a68db375afbd507706b35c832
owner <owner@10.18.151.8>
2015-01-29 09:17:08 -0700

    Commit for displaying thee map

--- JavaApplication2/src/escapePackage/Escape.java
+++ JavaApplication2/src/escapePackage/Escape.java
@@ -31,7 +31,7 @@ public class Escape {
         // call setter functions
         playerOne.setName("Fred Flintstone");
         gameOne.setTotalTime(120.5);
-        actorOne.setName("Jaba the Hut");
+        actorOne.setName("Jaba the Hutt");
         gameMap.setRowCount(10);
         gameMap.setColumnCount(5);
         itemOne.setItemType("Berries");
@@ -50,7 +50,8 @@ public class Escape {
         System.out.println(actorInfo);
 
         // display map
-        System.out.println(gameMap);
+        String mapInfo = gameMap.toString();
+        System.out.println(mapInfo);
         
         // display item
         String itemInfo = itemOne.toString();
