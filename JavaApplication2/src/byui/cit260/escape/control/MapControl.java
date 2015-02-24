ab4471f85d87c71a3cf0272b675dde43c4dddefc
ryanjoos <ryanjoos@10.18.149.18>
2015-02-24 08:43:06 -0700

    Getters and Setters are in for player and game.

--- JavaApplication2/src/byui/cit260/escape/control/MapControl.java
+++ JavaApplication2/src/byui/cit260/escape/control/MapControl.java
@@ -24,4 +24,23 @@ public class MapControl {
 
         return speedMPH;
     }
+    
+    public double calcBridgeSize(double length, double width, double height){
+        
+        if (length < 10 || length >15){ //If length is between 10 and 15, you're good to go.
+        return -1;
+        }
+        
+        if (width < 4 || width > 15){ //If width is between 9 & 15 it works.
+        return -1;
+        }
+        
+        if (height < 0.75 || height > 1){ //If height is between 0.75 and 1 then it works.
+        return -1;
+        }
+        
+        double bridgevolume = length * width * height; //The calculations for this function
+
+        return bridgevolume;   
+    }
 }
