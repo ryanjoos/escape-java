a9dcb7987827e5625182c9eb7d31a0fba09501a0
Carissa <Carissa@Carissa-PC>
2015-02-24 23:21:45 -0700

    The team assignment may be finished

--- JavaApplication2/src/byui/cit260/escape/view/SaveGameView.java
+++ JavaApplication2/src/byui/cit260/escape/view/SaveGameView.java
@@ -5,10 +5,54 @@
  */
 package byui.cit260.escape.view;
 
+import byui.cit260.escape.control.SaveGameControl;
+import byui.cit260.escape.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author ryanjoos
  */
 public class SaveGameView {
-    
+
+//    public void displayMenu() {
+//
+//        char selection = ' ';
+//        do {
+//
+//            System.out.println(MENU); // display main menu
+//
+//            String input = this.getInput();
+//            selection = input.charAt(0);
+//
+//            this.doAction(selection);
+//
+//        } while (selection != 'E');
+//    }
+
+    String getSaveInput() {
+        boolean valid = false; //Indicates if the name has be retrieved 
+        String inputValue = null;
+        Scanner keyboard = new Scanner(System.in); //keyboard input stream
+
+        while (!valid) { //While a valid name has not been retrieved
+
+            //Prompt for User name
+            System.out.println("\n Matey, which file would ye like to save ye game: ");
+
+            //get the name from the keyboard and trim off the branks
+            inputValue = keyboard.nextLine();
+            inputValue = inputValue.trim();
+
+            //If the name is invalid
+            if (inputValue.length() < 1) {
+                System.out.println("Please enter a file name ");
+                continue; // and repeat again
+            }
+            else 
+                SaveGameControl.saveGame();
+            break; // stop the do while thingy
+        }
+        return inputValue;
+    }
 }
