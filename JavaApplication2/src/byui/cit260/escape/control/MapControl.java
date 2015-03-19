0b619deef6f600d466ec544f1d6cd2c7fca0f642
Carissa <Carissa@10.18.151.185>
2015-03-19 09:11:39 -0600

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/byui/cit260/escape/view/GameMenuView.java

--- JavaApplication2/src/byui/cit260/escape/control/MapControl.java
+++ JavaApplication2/src/byui/cit260/escape/control/MapControl.java
@@ -6,6 +6,7 @@
 package byui.cit260.escape.control;
 
 import byui.cit260.escape.exceptions.MapControlException;
+import byui.cit260.escape.model.Actor;
 import byui.cit260.escape.model.Game;
 import byui.cit260.escape.model.Map;
 import byui.cit260.escape.model.Scene;
@@ -98,8 +99,8 @@ public class MapControl {
         int newRow = coordinates.x-1;
         int newColumn = coordinates.y-1;
         
-        if(newRow < 0 || newRow >= MAP_SIZE ||
-            newColumn < 0 || newColumn >= MAP_SIZE){
+        if(newRow < 0 || newRow >= map.getRowCount() ||
+            newColumn < 0 || newColumn >= map.getColumnCount()){
             throw new MapControlException("Can not move actor to location "
             + coordinates.x + ", " + coordinates.y
             + " because that location is outside "
